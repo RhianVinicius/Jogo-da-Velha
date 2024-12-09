e93067a249c4a1f1ecc54ea378682120dc0c043e
Rhian Vinícius <rianresende497@gmail.com>
2024-12-09 09:32:15 -0300

    Criar função matrixGenerator e atualizar função printBoard para aceitar as novas matrizes

--- lib/game-functions.dart
+++ lib/game-functions.dart
@@ -2,25 +2,72 @@ import "dart:io";
 import "utils.dart";
 
 
-void printBoard(Map matriz) {
+List<List<String>> matrixGenerator({int sideLength = 3}) {
+  List<List<String>> matrix = [];
 
-  int cont = 1, subCont = 1;
-  print("");
-  for (var entry in matriz.entries) {
-    if (cont != 3) {
-      stdout.write(" ${entry.value} │");
-    } else {
-      print(" ${entry.value} ");
+  int cont = 1;
+  for (int i = 0; i < sideLength; i++) {
+
+    List<String> matrixLine = [];
+    for (int i2 = 0; i2 < sideLength; i2++) {
+      matrixLine.add(cont.toString());
+      cont++;
     }
-    cont++;
+    matrix.add(matrixLine);
+  }
+  return matrix;
+}
 
-    if (cont == 4 && subCont != 3) {
-      print("―" * 11);
-      cont = 1;
-      subCont++;
-    } 
+
+void printBoard(List matrix) {
+  print("");
+
+  int contLine = 1;
+  for (List<String> matrixLine in matrix) {
+    // [1, 2, 3]
+    int lineLength = matrixLine.length; // 4
+
+    int cont = 1;
+    for (String val in matrixLine) {
+      if (cont != lineLength) {
+        stdout.write(" $val │");
+      } else {
+        print(" $val ");
+      }
+      cont++;
+    }
+    if (contLine != 3) {
+      print(("―――" * lineLength) + ("―" * (lineLength - 1)));
+    }
+    contLine++;
   }
   print("");
+
+
+
+
+
+
+
+
+
+  // int cont = 1, subCont = 1;
+  // print("");
+  // for (var entry in matriz.entries) {
+  //   if (cont != 3) {
+  //     stdout.write(" ${entry.value} │");
+  //   } else {
+  //     print(" ${entry.value} ");
+  //   }
+  //   cont++;
+
+  //   if (cont == 4 && subCont != 3) {
+  //     print("―" * 11);
+  //     cont = 1;
+  //     subCont++;
+  //   } 
+  //}
+  //print("");
 }
 
 
@@ -35,9 +82,9 @@ Map<String, String> askPlayersInfo({String signalChoice = "computer"}) {
     print("Qual o nome do seu oponente?");
     rivalName = notNullInput();
   } else if (signalChoice == "computer") {
-    rivalName = "PCzão Zero-bala ";
+    rivalName = "PCzão Zero-bala";
   } else {
-    throw ArgumentError("O parâmetro informado está INCORRETO\nos parâmetros possíveis são: \"computer\" ou \"player\'");
+    throw ArgumentError("O parâmetro informado na função askPlayersInfo(), em game-functions.dart está INCORRETO\nos parâmetros possíveis são: \"computer\" ou \"player\"");
   }
 
   print("\n$playerName, digite \"X\" para escolher X, e \"C\" para escolher círculo");
